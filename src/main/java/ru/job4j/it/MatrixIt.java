4388ab09212a9d01b618d4ef2c7d378e37368d4a
Andrew Petrushin <shmopls@gmail.com>
2022-03-16 18:36:34 +0500

    Итератор для двухмерного массива int

---
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -0,0 +1,81 @@
+package ru.job4j.it;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс реализует инерфейс
+ * @see Iterator для поэлементного прохода по двумерному массиву
+ * @author ANDREW PETRUSHIN (JOB4J Project)
+ * @version 1.0
+ */
+public class MatrixIt implements Iterator<Integer> {
+
+    /**
+     * Все данные храним в двумерном массиве data[][]
+     * Хождение по каждому измерению массива осуществляется благодаря полям row и column
+     */
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    /**
+     * Метод для проверки наличия следующего элемента (в естесственном порядке)
+     * В методе произведена проверка на наличие элементов в двух измерениях массива
+     * @return индексы двумерного массива (координаты элемента матрицы)
+     */
+    @Override
+    public boolean hasNext() {
+        while (row < data.length) {
+            if (column < data[row].length) {
+                break;
+            }
+            row++;
+            column = 0;
+        }
+        return row < data.length && column < data[row].length;
+    }
+
+    /**
+     * Метод для движения по элементам массива с инкрементацией поля column(в естесственном порядке)
+     */
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (column == data[row].length) {
+            row++;
+            column = 0;
+        }
+        return data[row][column++];
+    }
+//        if (!hasNext()) {
+//            new NoSuchElementException();
+//        }
+//        return data[row][column++];
+
+    @Override
+    public String toString() {
+        return "MatrixIt{"
+                + "row=" + Arrays.toString(data[row])
+                + ", column=" + Arrays.toString(data[column])
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        int[][] in = {
+                {1, 2},
+                {3, 4}
+        };
+        MatrixIt testMatrix = new MatrixIt(in);
+        testMatrix.hasNext();
+        testMatrix.next();
+        System.out.println(testMatrix);
+    }
+}
