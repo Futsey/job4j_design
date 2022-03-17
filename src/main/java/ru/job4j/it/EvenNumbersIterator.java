42b6c31ab78df339f86d42285ba94dd29b91888f
Andrew Petrushin <shmopls@gmail.com>
2022-03-17 15:46:11 +0500

    JavaDoc update

--- src/main/java/ru/job4j/it/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/it/EvenNumbersIterator.java
@@ -4,22 +4,43 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Класс реализует инерфейс
+ * @see Iterator для прохода по четным элементам плоского массива
+ * @author ANDREW PETRUSHIN (JOB4J Project)
+ * @version 1.0
+ */
 public class EvenNumbersIterator implements Iterator<Integer> {
 
     private int[] data;
     private int index;
 
+    /**
+     * Конструктор дополнен двумя основными полями:
+     * инициализирован индекс для корректного перемещения указателя при применении метода
+     * {@link #nextEven()}
+     */
     public EvenNumbersIterator(int[] data) {
         this.data = data;
         index = -1;
         nextEven();
     }
 
+    /**
+     * Метод для проверки наличия следующего элемента (в естесственном порядке)
+     * @return индекс массива данных
+     */
     @Override
     public boolean hasNext() {
         return index < data.length;
     }
 
+    /**
+     * Метод возвращает итерированный элемент (в естесственном порядке)
+     * Для фильтррации только четных элементов используем метод
+     * {@link #nextEven()}
+     * @return четный элемент массива
+     */
     @Override
     public Integer next() {
         if (!hasNext()) {
@@ -30,6 +51,10 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         return rsl;
     }
 
+    /**
+     * Метод - надстройка над методом
+     * {@link #next()} для фильтрации четных элементов массива (в естесственном порядке)
+     */
     private void nextEven() {
         index = index + 1;
         while (index < data.length && data[index] % 2 == 1) {
