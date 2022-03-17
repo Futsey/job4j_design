4d40fa90f246137f82ace9ff36b242c967b588b7
Andrew Petrushin <shmopls@gmail.com>
2022-03-17 19:50:40 +0500

    Создать итератор четные числа

--- src/main/java/ru/job4j/it/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/it/EvenNumbersIterator.java
@@ -15,51 +15,32 @@ public class EvenNumbersIterator implements Iterator<Integer> {
     private int[] data;
     private int index;
 
-    /**
-     * Конструктор дополнен двумя основными полями:
-     * инициализирован индекс для корректного перемещения указателя при применении метода
-     * {@link #nextEven()}
-     */
     public EvenNumbersIterator(int[] data) {
         this.data = data;
-        index = -1;
-        nextEven();
     }
 
     /**
      * Метод для проверки наличия следующего элемента (в естесственном порядке)
-     * @return индекс массива данных
+     * @return индекс четного элемента массива данных
      */
     @Override
     public boolean hasNext() {
+        while (index < data.length && data[index] % 2 == 1) {
+            index++;
+        }
         return index < data.length;
     }
 
     /**
      * Метод возвращает итерированный элемент (в естесственном порядке)
-     * Для фильтррации только четных элементов используем метод
-     * {@link #nextEven()}
-     * @return четный элемент массива
+     * @return следующий элемент массива
      */
     @Override
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException("No such element.");
         }
-        Integer rsl = data[index];
-        nextEven();
-        return rsl;
-    }
-
-    /**
-     * Метод - надстройка над методом
-     * {@link #next()} для фильтрации четных элементов массива (в естесственном порядке)
-     */
-    private void nextEven() {
-        index = index + 1;
-        while (index < data.length && data[index] % 2 == 1) {
-            index++;
-        }
+        return data[index++];
     }
 
     @Override
