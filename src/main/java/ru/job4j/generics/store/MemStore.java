010f3deec3c72776a891bb4fe5e8385581c04d3c
Andrew Petrushin <shmopls@gmail.com>
2022-03-23 18:38:35 +0500

    Реализовать Store<T extends Base>

--- src/main/java/ru/job4j/generics/store/MemStore.java
+++ src/main/java/ru/job4j/generics/store/MemStore.java
@@ -15,13 +15,13 @@ public class MemStore<T extends Base> implements Store<T> {
     @Override
     public boolean replace(String id, T model) {
         storage.replace(id, model);
-        return true;
+        return false;
     }
 
     @Override
     public boolean delete(String id) {
         storage.remove(id);
-        return true;
+        return false;
     }
 
     @Override
