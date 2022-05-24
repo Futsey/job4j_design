e3f4099508253be839838e3e2487e2a6a3ae30ca
Fut <shmopls@gmail.com>
2022-05-24 08:54:33 +0500

    Поиск дубликатов

--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -14,7 +14,11 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
-        allFiles.putIfAbsent(fileProperty, new ArrayList<>(List.of(file)));
+        if (!allFiles.containsKey(fileProperty)) {
+            allFiles.putIfAbsent(fileProperty, new ArrayList<>(List.of(file)));
+        } else {
+            allFiles.put(fileProperty, new ArrayList<>(List.of(file)));
+        }
         return super.visitFile(file, attrs);
     }
 
