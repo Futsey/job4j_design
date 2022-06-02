d9e107c629c29001d757cdbc4d741607a0e22d70
Fut <shmopls@gmail.com>
2022-06-02 19:07:13 +0500

    Архивировать проект

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -31,7 +31,7 @@ public class SearchFiles implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (condition.test(file)) {
-            visitList.add(file.toAbsolutePath());
+            visitList.add(file);
         }
         return CONTINUE;
     }
