02e81b99bb2f7049d83279fc9b932c82a277b7e6
Fut <shmopls@gmail.com>
2022-06-02 13:01:02 +0500

    Архивировать проект

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -2,12 +2,18 @@ package ru.job4j.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * Класс производит проверку и поиск файлов по определенному условию (Predicate).
+ * @author ANDREW PETRUSHIN (JOB4J Project)
+ * @version 1.0
+ */
 public class Search {
 
     public static void main(String[] args) throws IOException {
@@ -19,12 +25,24 @@ public class Search {
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
+    /**
+     * Метод для хождения по файловому дереву
+     * Через созданный экземпляр класса
+     * @see SearchFiles
+     * реализующего интерфейс
+     * @see java.nio.file.FileVisitor
+     * используем функционал метода
+     * {@link java.nio.file.Files#walkFileTree(Path, FileVisitor)}
+     */
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
 
+    /**
+     * Метод для проверки всех заданных по умолчанию аргументов
+     */
     public static void check(String[] args) {
         File path = new File(args[0]);
         String charToSearch = args[1];
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,14 +3,27 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * Класс производит архивацию данных.
+ * В классе реализованы два метода:
+ * для архивации одного файла
+ * {@link #packSingleFile(File, File)}
+ * для архивации директории (всех файлов в директории)
+ * {@link #packFiles(List, File)}
+ * @author ANDREW PETRUSHIN (JOB4J Project)
+ * @version 1.0
+ */
 public class Zip {
 
+    /**
+     * Метод для архивации директории (всех файлов в директории)
+     */
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File file: sources) {
@@ -25,6 +38,9 @@ public class Zip {
         }
     }
 
+    /**
+     * Метод для архивации одного файла
+     */
     public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
@@ -36,6 +52,13 @@ public class Zip {
         }
     }
 
+    /**
+     * Метод для проверки валидности предоставленных ключей
+     * В качестве ключа передаётся:
+     * -d - directory - директория, в которую мы хотим архивировать.
+     * -e - exclude - исключить файлы с расширением class.
+     * -o - output - адрес, куда мы архивируем.
+     */
     private static void validate(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("You have no arguments loaded");
@@ -48,7 +71,7 @@ public class Zip {
         if (!path.toFile().exists()) {
             throw new IllegalArgumentException("Source doesn't exist");
         }
-        if (!output.isEmpty()) {
+        if (!output.contains(".")) {
             throw new IllegalArgumentException("You have to add filename to write the data");
         }
         if (!exclude.startsWith(".")) {
@@ -56,14 +79,20 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );
         validate(args);
-        List<File> sources = List.of(new File("C:\\Test"));
-        zip.packFiles(sources, new File("C:\\Test\\zipped.zip"));
+        ArgsName argsName = ArgsName.of(args);
+        Path startDirectory = Path.of(argsName.get("d"));
+        Predicate<Path> pred = (p) -> p.toFile().getName().endsWith(argsName.get("e"));
+        List<Path> list = Search.search(startDirectory, pred);
+        Path target = Path.of(argsName.get("o"));
+        zip.packFiles(list.stream()
+                .map(Path::toFile)
+                .collect(Collectors.toList()), target.toFile());
     }
 }
