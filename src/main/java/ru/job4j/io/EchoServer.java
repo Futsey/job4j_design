40e154695995c8fefd8a3ef237dff2903b609f13
Fut <shmopls@gmail.com>
2022-06-08 11:22:23 +0500

    Что такое Socket?

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,7 +3,6 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
 public class EchoServer {
 
@@ -15,19 +14,15 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (checkForWord(str, "Bye")) {
+                    String str = in.readLine();
+                    String finish = "Bye";
+                        if (str.contains(finish)) {
                             server.close();
                         }
                         System.out.println(str);
+                        out.flush();
                     }
-                    out.flush();
-                }
             }
         }
-    }
-
-    public static boolean checkForWord(String line, String word) {
-        return line.contains(word);
     }
 }
