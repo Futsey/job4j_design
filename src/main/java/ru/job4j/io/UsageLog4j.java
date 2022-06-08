c81868b294f0338912f0768d9bd39a4937631aae
Fut <shmopls@gmail.com>
2022-06-08 16:50:01 +0500

    Slf4j - вывод переменных

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,10 +9,33 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        BasicConfigurator.configure();
+        String name = "Petr Arsentev";
+        int age = 33;
+        short apartmentSize = 69;
+        byte children = 2;
+        long students = 100500;
+        float workEfficiency = 89.3F;
+        double relaxEfficiency = 146.3D;
+        char firstLiteralOfAlphabet = 'A';
+        boolean alive = true;
+
+        LOG.debug("User info name : {}, "
+                        + "age : {}, "
+                        + "apartmentSize : {}, "
+                        + "children : {}, students : {}, "
+                        + "workEfficiency : {}, "
+                        + "relaxEfficiency : {}, "
+                        + "firstLiteralOfAlphabet : {}, "
+                        + "age : {}",
+                name,
+                age,
+                apartmentSize,
+                children,
+                students,
+                workEfficiency,
+                relaxEfficiency,
+                firstLiteralOfAlphabet,
+                alive);
     }
 }
