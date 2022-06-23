a5b39e158fdc7b10aea4599450cc2ea315d8c52e
Fut <shmopls@gmail.com>
2022-06-23 09:21:31 +0500

    Схема прав пользователи и роли. [#1093 #314374]

--- src/main/java/ru/job4j/sql/users/create.sql
+++ src/main/java/ru/job4j/sql/users/create.sql
@@ -1,3 +1,5 @@
+Правило!!! Внешний ключ всегда ставится со стороны many
+
 create table new_rules (
     id serial primary key,
     name text
@@ -11,35 +13,36 @@ create table new_role (
 create table new_user (
     id serial primary key,
     name varchar(255),
-    role_id int references new_role(id)
+    fk_role_id int references new_role(id)
 );
 
 create table new_comments (
     id serial primary key,
     name text
+    fk_item_id int references new_item(id)
 );
 
 create table new_attachs (
     id serial primary key,
-    name text
+    name text,
+    fk_item_id int references new_item(id)
 );
 
 create table new_item (
     id serial primary key,
     name text,
-    user_id int references new_user(id),
-    comments_id int references new_comments(id),
-    attachs_id int references new_attachs(id)
+    fk_user_id int references new_user(id)
+    fk_new_category_id int references new_category(id)
+    fk_new_state_id int references new_state(id)
 );
 
 create table new_category (
     id serial primary key,
-    name text,
-    item int references new_item(id)
+    name text
 );
 
 create table new_state (
     id serial primary key,
     name text,
-    tem int references new_item(id)
+
 );
