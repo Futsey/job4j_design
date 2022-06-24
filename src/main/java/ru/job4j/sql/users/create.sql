56c67926d8edc2a7d7fca5f612e49e87538e1625
Fut <shmopls@gmail.com>
2022-06-24 10:22:17 +0500

    Схема прав пользователи и роли.

--- src/main/java/ru/job4j/sql/users/create.sql
+++ src/main/java/ru/job4j/sql/users/create.sql
@@ -10,30 +10,11 @@ create table new_role (
     role text
 );
 
-create table new_user (
+create table rule_role (
     id serial primary key,
-    name varchar(255),
-    fk_role_id int references new_role(id)
-);
-
-create table new_comments (
-    id serial primary key,
-    name text
-    fk_item_id int references new_item(id)
-);
-
-create table new_attachs (
-    id serial primary key,
-    name text,
-    fk_item_id int references new_item(id)
-);
-
-create table new_item (
-    id serial primary key,
-    name text,
-    fk_user_id int references new_user(id)
-    fk_new_category_id int references new_category(id)
-    fk_new_state_id int references new_state(id)
+    rule_role text,
+    new_rules_id int references new_rules(id),
+    new_role_id int references new_role(id)
 );
 
 create table new_category (
@@ -43,6 +24,32 @@ create table new_category (
 
 create table new_state (
     id serial primary key,
-    name text,
-
+    name text
 );
+
+create table new_user (
+    id serial primary key,
+    name varchar(255),
+    fk_role_id int references new_role(id)
+);
+
+create table new_item (
+    id serial primary key,
+    name text,
+    fk_user_id int references new_user(id),
+    fk_new_category_id int references new_category(id),
+    fk_new_state_id int references new_state(id)
+);
+
+create table new_comments (
+    id serial primary key,
+    name text,
+    fk_item_id int references new_item(id)
+);
+
+create table new_attachs (
+    id serial primary key,
+    name text,
+    fk_item_id int references new_item(id)
+);
+
