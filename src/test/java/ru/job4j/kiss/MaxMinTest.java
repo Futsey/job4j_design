1f2ba9b6880c40761fefd6f54165f4d9bb0aac1e
Fut <shmopls@gmail.com>
2022-08-09 19:47:27 +0500

    Принципы Kiss, Dry и Yagni

--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MaxMinTest {
 
-    private List<Integer> value = List.of(1,2,3,4,5,6,7,8,9,10,11,12);
+    private List<Integer> value = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
     private Comparator<Integer> comparator = Comparator.naturalOrder();
     private MaxMin maxMin = new MaxMin();
 
