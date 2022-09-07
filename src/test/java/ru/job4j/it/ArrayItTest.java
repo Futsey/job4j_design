70200c5013331825b13e20fb1e40f073e62dc99b
Fut <shmopls@gmail.com>
2022-09-07 13:36:33 +0500

    Подключение библиотеки AssertJ

--- src/test/java/ru/job4j/it/ArrayItTest.java
+++ src/test/java/ru/job4j/it/ArrayItTest.java
@@ -1,14 +1,14 @@
 package ru.job4j.it;
 
-import org.junit.Test;
-
-import java.util.NoSuchElementException;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 public class ArrayItTest {
 
+    /**
+     * JUnit 4 test
+
+    @Deprecated
     @Test
     public void whenMultiCallhasNextThenTrue() {
         ArrayIt it = new ArrayIt(
@@ -18,6 +18,8 @@ public class ArrayItTest {
         assertThat(it.hasNext(), is(true));
     }
 
+     * JUnit 4 test
+    @Deprecated
     @Test
     public void whenReadSequence() {
         ArrayIt it = new ArrayIt(
@@ -28,11 +30,35 @@ public class ArrayItTest {
         assertThat(it.next(), is(3));
     }
 
+     * JUnit 4 test
+
+    @Deprecated
     @Test(expected = NoSuchElementException.class)
     public void whenNextFromEmpty() {
         ArrayIt it = new ArrayIt(
                 new int[] {}
         );
         it.next();
+    }
+     */
+
+    @Test
+    void whenMultiCallHasNextThenTrueJUnit5() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        boolean rsl = it.hasNext();
+        assertThat(rsl).isTrue();
+        assertThat(it.hasNext()).isTrue();
+    }
+
+    @Test
+    void whenReadSequenceJUnit5() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+        assertThat(it.next()).isEqualTo(3);
     }
 }
